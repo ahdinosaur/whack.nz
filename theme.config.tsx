ea11d741ac286ced04a6c11b27d2fe477c961900
Michael Williams <hello@mikey.nz>
2024-09-08 21:58:13 +1200

    Let's go

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/wackerspace/wackerspace.com',
   footer: {
-    text: 'Imaginary Wellington Hackerspace',
+    text: 'Hacking is about making things, creating technology and solving problems with innovative solutions. Hackerspaces facilitate hacking through providing a community, a workshop and tools.',
   },
 }
 
