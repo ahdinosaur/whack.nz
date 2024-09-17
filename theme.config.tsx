aeed72bcdc00b1ca29049c1f666238009e4d334d
Michael Williams <hello@mikey.nz>
2024-09-17 13:51:30 +1200

    Another blast

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,11 +1,10 @@
-import { useRouter } from 'next/router'
 import { type DocsThemeConfig, useConfig } from 'nextra-theme-docs'
 import React from 'react'
 
 const config: DocsThemeConfig = {
   logo: <span>WHACK.NZ</span>,
   project: {
-    link: 'https://github.com/ahdinosaur/whack.nz',
+    link: 'https://github.com/whacknz/whack',
   },
   chat: {
     link: 'https://discord.gg/S6RFwQYH',
@@ -15,15 +14,15 @@ const config: DocsThemeConfig = {
     const { frontMatter } = useConfig()
 
     return {
-      title: frontMatter.title ?? 'Wellington Hackspace',
+      title: frontMatter.title ?? 'WHACK: Wellington Hackspace',
       description:
         frontMatter.description ??
-        'Imagine a Wellington community space for anyone to make, learn, and hang out. Members have access to 3d printers, sewing machines, laser cutters, CNC machines, soldering stations, and more.',
+        'Imagine a Wellington community space for anyone to make, learn and hang out. Members have access to 3d printers, sewing machines, laser cutters, CNC machines, soldering stations and more.',
       openGraph: {
         type: 'website',
         local: 'en_US',
         url: 'https://whack.nz',
-        siteName: 'Wellington Hackspace',
+        siteName: 'WHACK: Wellington Hackspace',
         images: [
           {
             url: 'https://whack.nz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhackspace.efbfd34b.jpg&w=1080&q=75',
